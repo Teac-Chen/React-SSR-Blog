86e4c38ef17d329f6298ccac041e58e5533591e3
chtic123 <87691805@qq.com>
2018-01-16 23:17:54 +0800

    modify the gulpfile.js to distinguish the NODE_ENV

--- gulpfile.js
+++ gulpfile.js
@@ -3,36 +3,49 @@ const $ = require('gulp-load-plugins')();
 
 const compileFile = ['./src/server/**/*.js'];
 
-gulp.task('lint', () => {
-  return gulp.src(compileFile)
-    .pipe($.eslint())
-    .pipe($.eslint.format())
-    .pipe($.eslint.failAfterError());
-});
-
-gulp.task('compile', ['lint'], () => {
-  gulp.src(compileFile)
-    .pipe($.babel({
-      presets: ['env'],
-      plugins: ['transform-runtime']
-    }))
-    .pipe(gulp.dest('./bin/server'))
-});
-
-gulp.task('server', ['compile'], () => {
-  let stream = $.nodemon({
-    script: 'bin/server/app.js',
-    watch: ["bin/server"],
-    delay: 1000
+if(process.env.NODE_ENV === 'development'){
+  gulp.task('lint', () => {
+    return gulp.src(compileFile)
+      .pipe($.eslint())
+      .pipe($.eslint.format())
+      .pipe($.eslint.failAfterError());
   });
 
-  return stream
-})
+  gulp.task('compile', ['lint'], () => {
+    gulp.src(compileFile)
+      .pipe($.babel({
+        presets: ['env'],
+        plugins: ['transform-runtime']
+      }))
+      .pipe(gulp.dest('./bin/server'))
+  });
 
-gulp.task('default', ['server']);
+  gulp.task('server', ['compile'], () => {
+    let stream = $.nodemon({
+      script: 'bin/server/app.js',
+      watch: ["bin/server"],
+      delay: 1000
+    });
 
-const watcher = gulp.watch(compileFile, ['compile']);
+    return stream
+  })
 
-watcher.on('change', e => {
-  console.log('File ' + e.path + ' was ' + e.type + ', running tasks...');
-})
+  gulp.task('default', ['server']);
+
+  const watcher = gulp.watch(compileFile, ['compile']);
+
+  watcher.on('change', e => {
+    console.log('File ' + e.path + ' was ' + e.type + ', running tasks...');
+  })
+}
+
+if(process.env.NODE_ENV === 'production'){
+  gulp.task('default', () => {
+    gulp.src(compileFile)
+      .pipe($.babel({
+        presets: ['env'],
+        plugins: ['transform-runtime']
+      }))
+      .pipe(gulp.dest('./bin/server'))
+  });
+}
